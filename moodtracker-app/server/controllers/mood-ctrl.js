ccb9d1ca0f236d737465687fb0ef6ffd11a79c19
Remzilla <remington.manso@johnlewis.co.uk>
2020-07-13 08:47:53 +0100

    added routers and controllers

---
+++ moodtracker-app/server/controllers/mood-ctrl.js
@@ -0,0 +1,119 @@
+const Mood = require("../models/moods-model");
+
+createMood = (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a mood >:(",
+    });
+  }
+
+  const mood = new Mood(body);
+
+  if (!mood) {
+    return res.status(400).json({ success: false, error: err });
+  }
+
+  mood
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        id: mood._id,
+        message: "Mood created!",
+      });
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        error,
+        message: "Mood not created!",
+      });
+    });
+};
+
+updateMood = async (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a body to update",
+    });
+  }
+
+  Mood.findOne({ _id: req.params.id }, (err, mood) => {
+    if (err) {
+      return res.status(404).json({
+        err,
+        message: "Mood not found!",
+      });
+    }
+    mood.name = body.name;
+    mood.time = body.time;
+    mood.rating = body.rating;
+    mood
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: mood._id,
+          message: "Mood updated!",
+        });
+      })
+      .catch((error) => {
+        return res.status(404).json({
+          error,
+          message: "Mood not updated!",
+        });
+      });
+  });
+};
+
+deleteMood = async (req, res) => {
+  await Mood.findOneAndDelete({ _id: req.params.id }, (err, mood) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+
+    if (!mood) {
+      return res.status(404).json({ success: false, error: `Mood not found` });
+    }
+
+    return res.status(200).json({ success: true, data: mood });
+  }).catch((err) => console.log(err));
+};
+
+getMoodById = async (req, res) => {
+  await Mood.findOne({ _id: req.params.id }, (err, mood) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+
+    if (!mood) {
+      return res.status(404).json({ success: false, error: `Mood not found` });
+    }
+    return res.status(200).json({ success: true, data: mood });
+  }).catch((err) => console.log(err));
+};
+
+getMood = async (req, res) => {
+  await Mood.find({}, (err, mood) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+    if (!mood.length) {
+      return res.status(404).json({ success: false, error: `Mood not found` });
+    }
+    return res.status(200).json({ success: true, data: mood });
+  }).catch((err) => console.log(err));
+};
+
+module.exports = {
+  createMood,
+  updateMood,
+  deleteMood,
+  getMood,
+  getMoodById,
+};
--- moodtracker-app/server/models/moods-model.js
+++ moodtracker-app/server/models/moods-model.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const Mood = new Schema(
-    {
-        id: { type: Number, required: true },
-        mood_type: { type: String, required: true },
-    },
-    { timestamps: true },
-)
+  {
+    mood_type: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('moods', Mood)
+module.exports = mongoose.model("moods", Mood);
