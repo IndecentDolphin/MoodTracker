0642f702f0be9141a7a25b395b772452ce394a17
Remzilla <remington.manso@johnlewis.co.uk>
2020-07-17 14:32:13 +0100

    WIP: connected to the db, trying to display the value is tricky

--- moodtracker-app/client/src/pages/MoodList.jsx
+++ moodtracker-app/client/src/pages/MoodList.jsx
@@ -1,14 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import api from "../api";
+
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 0 40px 40px 40px;
+`;
 
 class MoodsList extends Component {
-    render() {
-        console.log('here')
-        return (
-            <div>
-                <p>In this page you'll see the list of moods</p>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      moods: [],
+      isLoading: false,
+    };
+  }
+
+  componentDidMount = async () => {
+    this.setState({ isLoading: true });
+
+    await api.getAllMoods().then((moods) => {
+      this.setState({
+        moods: moods.data.data,
+        isLoading: false,
+      });
+    });
+  };
+
+  render() {
+    const { moods } = this.state;
+    console.log("TLC: MoodsList -> render -> moods", moods);
+
+    return (
+      <Wrapper>
+        <div>{moods && <p>These are the moods {moods.mood_type}</p>}</div>
+      </Wrapper>
+    );
+  }
 }
 
-export default MoodsList
+export default MoodsList;
