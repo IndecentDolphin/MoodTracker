b0c1422265f84719bad95cdf79cce2a802cd9dd6
Remzilla <remington.manso@johnlewis.co.uk>
2020-08-07 10:56:31 +0100

    WIP: Updated MoodInsert, close, I believe we are getting an error on submit that stops the api call

--- moodtracker-app/client/src/pages/MoodInsert.jsx
+++ moodtracker-app/client/src/pages/MoodInsert.jsx
@@ -1,13 +1,83 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import api from "../api";
 
-class MoodsInsert extends Component {
-    render() {
-        return (
-            <div>
-                <p>In this page you'll see the form to add a moods</p>
-            </div>
-        )
-    }
+import styled from "styled-components";
+
+const Title = styled.h1.attrs({
+  className: "h1",
+})``;
+
+const Wrapper = styled.div.attrs({
+  className: "form-group",
+})`
+  margin: 0 30px;
+`;
+
+const Label = styled.label`
+  margin: 5px;
+`;
+
+const InputText = styled.input.attrs({
+  className: "form-control",
+})`
+  margin: 5px;
+`;
+
+const Button = styled.button.attrs({
+  className: `btn btn-primary`,
+})`
+  margin: 15px 15px 15px 5px;
+`;
+
+const CancelButton = styled.a.attrs({
+  className: `btn btn-danger`,
+})`
+  margin: 15px 15px 15px 5px;
+`;
+
+class MoodInsert extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mood_type: "",
+    };
+  }
+
+  handleChangeInputName = async (event) => {
+    const name = event.target.value;
+    this.setState({ name });
+  };
+
+  handleIncludeMood = async () => {
+    const { name } = this.state;
+    const payload = { name };
+
+    await api.insertMood(payload).then((res) => {
+      window.alert(`Movie inserted successfully`);
+      this.setState({
+        name: "",
+      });
+    });
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <Wrapper>
+        <Title>Create Mood</Title>
+
+        <Label>Name: </Label>
+        <InputText
+          type="text"
+          value={name}
+          onChange={this.handleChangeInputName}
+        />
+        <Button onClick={this.handleIncludeMood}>Add Mood</Button>
+        <CancelButton href={"/moods/list"}>Cancel</CancelButton>
+      </Wrapper>
+    );
+  }
 }
 
-export default MoodsInsert
+export default MoodInsert;
