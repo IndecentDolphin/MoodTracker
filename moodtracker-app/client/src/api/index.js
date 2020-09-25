6dc4995fd2d3922c5fdacec8e06095820fcd7777
Remzilla <remington.manso@johnlewis.co.uk>
2020-09-25 14:54:08 +0100

    changed the baseURL to our gcp hostname. Changed app title in Links

--- moodtracker-app/client/src/api/index.js
+++ moodtracker-app/client/src/api/index.js
@@ -1,21 +1,21 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
-})
+  baseURL: "https://moodtrackerapp.ew.r.appspot.com/api",
+});
 
-export const insertMood = payload => api.post(`/mood`, payload)
-export const getAllMoods = () => api.get(`/moods`)
-export const updateMoodById = (id, payload) => api.put(`/mood/${id}`, payload)
-export const deleteMoodById = id => api.delete(`/mood/${id}`)
-export const getMoodById = id => api.get(`/mood/${id}`)
+export const insertMood = (payload) => api.post(`/mood`, payload);
+export const getAllMoods = () => api.get(`/moods`);
+export const updateMoodById = (id, payload) => api.put(`/mood/${id}`, payload);
+export const deleteMoodById = (id) => api.delete(`/mood/${id}`);
+export const getMoodById = (id) => api.get(`/mood/${id}`);
 
 const apis = {
-    insertMood,
-    getAllMoods,
-    updateMoodById,
-    deleteMoodById,
-    getMoodById,
-}
+  insertMood,
+  getAllMoods,
+  updateMoodById,
+  deleteMoodById,
+  getMoodById,
+};
 
-export default apis
+export default apis;
